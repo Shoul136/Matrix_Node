69f72e6b6f275e5cc93cf09e882bd3293d6ac8cd
Luis Roque <luisrqvgdev@hotmail.com>
2026-04-10 13:05:11 -0700

    chore(server): Archivos de configuración sequelize faltantes. #5

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,21 @@
 import express from 'express'
 import morgan from 'morgan'
+import db from './config/db.js'
+import colors from 'colors'
 
+export async function connectDB()
+{
+    try {
+        await db.authenticate()
+        await db.sync({alter: true})
+        console.log(colors.blue('Conexión exitosa a la BD'))
+    } catch (error) {
+        console.log(colors.red.bold('Hubo un error al conectar la BD'));
+        console.log(error)
+    }
+}
+
+await connectDB()
 const server = express()
 
 server.use(express.json())
